f56b261ee9c8e22af0831b20bec74d90bed538be
Franco Peralta <franco.peralta7@hotmail.com>
2021-06-01 22:09:15 -0300

    Delete ui in actions

--- src/actions/ui.js
+++
@@ -1,4 +0,0 @@
-import { types } from '../types/types';
-
-export const uiOpenModalInflow = () => ({ type: types.uiOpenModalInflow});
-export const uiCloseModalInflow = () => ({ type: types.uiCloseModalInflow});
